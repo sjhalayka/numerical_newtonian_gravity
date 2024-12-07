127c1de3002f1db34fd5a45199b0fbbc7f8641a1
Shawn Halayka <sjhalayka@gmail.com>
2024-12-07 12:59:31 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,92 +1,214 @@
 #include "main.h"
 
 
-long long unsigned int get_intersecting_line_count(
-	const vector<vector_3>& unit_vectors,
+
+
+
+
+bool circle_intersect(vector_3 location, const vector_3 normal, const real_type circle_location, const real_type circle_radius)
+{
+	if (normal.dot(circle_location) <= 0)
+		return false;
+
+	vector_3 v = normal;
+
+	const real_type ratio = v.x / circle_location;
+
+	const vector_3 circle_origin(circle_location, 0, 0);
+
+	v.y = v.y / ratio;
+	v.z = v.z / ratio;
+	v.x = circle_location;
+
+	vector_3 v2;
+	v2.x = circle_origin.x - v.x;
+	v2.y = circle_origin.y - v.y;
+	v2.z = circle_origin.z - v.z;
+
+	if (v2.length() > circle_radius)
+		return false;
+
+	return true;
+}
+
+
+
+long long signed int get_intersecting_line_count(
+	const size_t n,
 	const vector_3 sphere_location,
 	const real_type sphere_radius)
 {
-	long long unsigned int count = 0;
+	long long signed int count = 0;
 
-	vector_3 cross_section_edge_dir(sphere_location.x, sphere_radius, 0);
-	cross_section_edge_dir.normalize();
+	for (size_t i = 0; i < n; i++)
+	{
+		vector_3 pos = RandomUnitVector();
+		vector_3 normal = pos;
 
-	vector_3 receiver_dir(sphere_location.x, 0, 0);
-	receiver_dir.normalize();
-
-	const real_type min_dot = cross_section_edge_dir.dot(receiver_dir);
-
-	for (size_t i = 0; i < unit_vectors.size(); i++)
-		if (unit_vectors[i].dot(receiver_dir) >= min_dot)
+		if (circle_intersect(pos, normal, sphere_location.x, sphere_radius))
 			count++;
+	}
 
 	return count;
 }
 
 
 
-real_type get_intersecting_line_count(
-	const real_type n,
-	const vector_3 sphere_location,
-	const real_type sphere_radius)
-{
-	const real_type big_area = 4 * sphere_location.x * sphere_location.x;
-	const real_type small_area = sphere_radius * sphere_radius;
-	const real_type ratio = small_area / big_area;
-	
-	return n * ratio;
-}
 
+
+
+
+
+//
+//real_type get_intersecting_line_count(
+//	const real_type n,
+//	const vector_3 sphere_location,
+//	const real_type sphere_radius)
+//{
+//	const real_type big_area =
+//		4 * pi
+//		* sphere_location.x * sphere_location.x;
+//
+//	const real_type small_area =
+//		pi
+//		* sphere_radius * sphere_radius;
+//
+//	const real_type ratio =
+//		small_area
+//		/ big_area;
+//
+//	return n * ratio;
+//}
+
+//
+//bool circle_intersect(vector_3 location, const vector_3 normal, const real_type circle_location, const real_type circle_radius)
+//{
+//	if (normal.dot(circle_location) <= 0)
+//		return false;
+//
+//	vector_3 v = normal;
+//
+//	const real_type ratio = v.x / circle_location;
+//
+//	const vector_3 circle_origin(circle_location, 0, 0);
+//
+//	v.y = v.y / ratio;
+//	v.z = v.z / ratio;
+//	v.x = circle_location;
+//
+//	vector_3 v2;
+//	v2.x = circle_origin.x - v.x;
+//	v2.y = circle_origin.y - v.y;
+//	v2.z = circle_origin.z - v.z;
+//
+//	if (v2.length() > circle_radius)
+//		return false;
+//
+//	return true;
+//}
+
+//
+//
+//size_t get_intersecting_line_count(
+//	//const vector<vector_3>& unit_positions,
+//	//const vector<vector_3>& unit_directions,
+//	size_t n,
+//	const vector_3 sphere_location,
+//	const real_type sphere_radius)
+//{
+//	size_t count = 0;
+//
+//	for (size_t i = 0; i < n; i++)
+//	{
+//		vector_3 pos = RandomUnitVector();
+//		vector_3 normal = pos;
+//
+//		if (circle_intersect(pos, normal, sphere_location.x, sphere_radius))
+//			count++;
+//	}
+//
+//	return count;
+//}
 
 int main(int argc, char** argv)
 {
+	const real_type receiver_radius = 1.0;
+	real_type emitter_radius = sqrt((10000000 * G * hbar * log(2.0)) / (k * c3 * pi));
+
+	const real_type emitter_area =
+		4.0 * pi * emitter_radius * emitter_radius;
+
 	// Field line count
-	//const size_t n = 100000000;
-	const real_type n = 1e70;
+	// re: holographic principle:
+	const real_type n =
+		(k * c3 * emitter_area)
+		/ (log(2.0) * 4.0 * G * hbar);
 
-	//cout << "Allocating memory for field lines" << endl;
-	//vector<vector_3> unit_vectors(n);
+	const real_type emitter_mass = c2 * emitter_radius / (2.0 * G);
 
-	//for (size_t i = 0; i < n; i++)
-	//{
-	//	unit_vectors[i] = RandomUnitVector();
+	// 2.39545e47 is the 't Hooft-Susskind constant:
+	// the number of field lines for a black hole of
+	// unit Schwarzschild radius
+	//
+	//const real_type G_ = 
+	//	(k * c3 * pi) 
+	//	/ (log(2.0) * hbar * 2.39545e47);
 
-	//	static const size_t output_mod = 10000;
-
-	//	if (i % output_mod == 0)
-	//		cout << "Getting pseudorandom locations: " << static_cast<float>(i) / n << endl;
-	//}
-
-
-	string filename = "newton.txt";
+	const string filename = "newton.txt";
 	ofstream out_file(filename.c_str());
 	out_file << setprecision(30);
 
-	const real_type start_distance = 10;
-	const real_type end_distance = 100;
-	const size_t distance_res = 1000;
-
-	const real_type distance_step_size = (end_distance - start_distance) / (distance_res - 1);
+	const real_type start_distance = (emitter_radius + receiver_radius);
+	const real_type end_distance = 100.0;
+	const size_t distance_res = 100;
+		
+	const real_type distance_step_size =
+		(end_distance - start_distance)
+		/ (distance_res - 1);
 
 	for (size_t step_index = 0; step_index < distance_res; step_index++)
 	{
-		const real_type r = start_distance + step_index * distance_step_size;
+		const real_type r =
+			start_distance + step_index * distance_step_size;
 
 		const vector_3 receiver_pos(r, 0, 0);
-		const real_type receiver_radius = 1;
 
-		const real_type epsilon = 0.1;
+		const real_type epsilon = 1.0;
 
 		vector_3 receiver_pos_plus = receiver_pos;
 		receiver_pos_plus.x += epsilon;
 
-		// https://en.wikipedia.org/wiki/Directional_derivative
-		const real_type collision_count_plus = get_intersecting_line_count(n, receiver_pos_plus, receiver_radius);
-		const real_type collision_count = get_intersecting_line_count(n, receiver_pos, receiver_radius);
-		const real_type gradient = (collision_count_plus - collision_count) / epsilon;
-		const real_type gradient_strength = -gradient / (4 * pi * receiver_radius * receiver_radius);
+		const long long signed int collision_count_plus =
+			get_intersecting_line_count(
+				static_cast<size_t>(n),
+				receiver_pos_plus,
+				receiver_radius);
 
-		cout << "r: " << r << " gradient strength: " << gradient_strength << endl;
+		const long long signed int collision_count =
+			get_intersecting_line_count(
+				static_cast<size_t>(n),
+				receiver_pos,
+				receiver_radius);
+
+		const real_type gradient =
+			(static_cast<real_type>(collision_count_plus) - static_cast<real_type>(collision_count))
+			/ epsilon;
+
+		const real_type gradient_strength =
+			-gradient
+			/ (receiver_radius * receiver_radius);
+
+		const real_type newton_strength =
+			G * emitter_mass / ( pow(receiver_pos.x, 2.0));
+
+		const real_type newton_strength_ =
+			gradient_strength * receiver_pos.x * c * hbar * log(2)
+			/ (k * 2 * pi * emitter_mass);
+
+		cout << newton_strength / newton_strength_ << endl;
+
+		cout << "r: " << r << " gradient strength: "
+			<< gradient_strength << endl;
 
 		out_file << r << " " << gradient_strength << endl;
 	}
